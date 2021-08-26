8855f14a7b35994396c8c3b09f045fcb7da2f3be
yk-amarly-20 <y.k.83600260@icloud.com>
2021-08-26 23:40:12 +0900

    main処理テスト

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,9 @@
 use actix_web::{get, App, HttpServer, Responder};
+mod controller;
+use controller::collect_infected_data::collect_infected_num;
+use controller::infected_prefectures::{
+    get_num_of_infected_over_jp, get_num_of_infected_prefecture,
+};
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -7,8 +12,14 @@ async fn index() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(get_num_of_infected_over_jp)
+            .service(get_num_of_infected_prefecture)
+            .service(collect_infected_num)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
